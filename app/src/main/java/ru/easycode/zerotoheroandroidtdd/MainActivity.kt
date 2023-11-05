65e9f038e6ce5075603ab073e5410a34307bff0f
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2023-11-05 05:44:01 +0200

    test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,32 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        val button = findViewById<Button>(R.id.hideButton)
+
+        if (savedInstanceState != null) {
+            textView.isVisible = savedInstanceState.getBoolean("TextVisibility")
+        }
+
+        button.setOnClickListener {
+            textView.isVisible = !textView.isVisible
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean("TextVisibility", textView.isVisible)
     }
 }
