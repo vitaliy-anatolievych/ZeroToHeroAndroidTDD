d1564bda3f8c569ee6ab7eb48ef024fc626641ff
vitaliy-anatolievych <vitaliy.anatolyevich@gmail.com>
2024-01-12 20:33:26 +0200

    test passed

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,50 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var list = ArrayList<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding =
+            ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        if (savedInstanceState != null) {
+            list = savedInstanceState.getStringArrayList(TAG) as ArrayList<String>
+        }
+
+        with(binding) {
+            if (list.isNotEmpty()) {
+                list.forEach { text ->
+                    val view = TextView(this@MainActivity)
+                    view.text = text
+
+                    contentLayout.addView(view)
+                }
+            }
+
+            actionButton.setOnClickListener {
+                val view = TextView(this@MainActivity)
+                val text = inputEditText.text.toString()
+                view.text = text
+                inputEditText.setText("")
+                list.add(text)
+
+                contentLayout.addView(view)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putStringArrayList(TAG, list)
+    }
+
+    companion object {
+        const val TAG = "tag"
     }
 }
